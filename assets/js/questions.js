54b8e5535bb6bc669c5bde6697fd1eb7c53fc6f5
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-06-20 12:41:53 -0700

    added first four questions to questions.js file.

---
+++ assets/js/questions.js
@@ -0,0 +1,21 @@
+const questions = [
+    {
+        question: 'Inside which HTML element do we put the JavaScript?',
+        choices: ['<scripting>', '<js>', '<javascript>', '<script>'],
+        answer: '<script>'
+    },{
+        question: 'Where is the correct place to insert a JavaScript?',
+        choices: ['The <body> section', 'Both the <head> section adn the <body> section are correct>', 'The <head> section', 'Nowhere'],
+        answer: 'Both the <head> section adn the <body> section are correct>'
+    },{
+        question: 'What is the correct syntax for referring to an external script called "xxx.js"?',
+        choices: ['<script name="xxx.js">', '<script href="xxx.js">', '<script type="xxx.js">', '<script src="xxx.js">'],
+        answer: '<script src="xxx.js">'
+    },{
+        question: 'How do you write "Hello World" in an alert box?',
+        choices: ['alertBox("Hello World");', 'msg("Hello World");', 'alert("Hello World");', 'msgBox("Hello World");'],
+        answer: 'alert("Hello World");'
+    },{
+        
+    }
+]
