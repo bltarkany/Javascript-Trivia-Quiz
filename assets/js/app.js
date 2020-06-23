9f9e184279d1c66e34ce2b9dfb0b0bd7920785ad
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-06-23 09:11:32 -0700

    Added results feedback section to the index.html file. Added variables to grab divs by ID in app.js.

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,7 +8,13 @@ var timer;
 // html #id grab
 const start = document.getElementById('start');
 const submit = document.getElementById('submit');
-
+const timeContainer = document.getElementById('time');
+const game = document.getElementById('game');
+const questionContainer = document.getElementById('question');
+const buttons = document.getElementById('btns');
+const result = document.getElementById('result');
+const finalScore = document.getElementById('final-score');
+const initialsContainer = document.getElementById('initials');
 
 
 
