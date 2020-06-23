921cff090c997bf57db4d20e1e1f99f843bebd6b
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-06-23 14:10:55 -0700

    created function to display high scores from local storage.

---
+++ assets/js/highscore.js
@@ -0,0 +1,26 @@
+// function to grab local storage winners and display
+function printScores() {
+    // grab scores from local storage
+    var highScores = JSON.parse(window.localStorage.getItem("highScores")) || [];
+    console.log(highScores);
+    // sort high scores in descending order
+    highScores.sort(function(a, b){
+        return b.score - a.score
+    });
+    console.log(highScores);
+    // loop through the json list of high scores
+    highScores.forEach(function(score) {
+        // create list item element
+        var player = document.createElement('li');
+        player.textContent = score.initials + ' -- ' + score.score;
+
+        // display list 
+        var winners = document.getElementById('winners');
+        winners.appendChild(player);
+    });
+}
+
+// function to clear high scores 
+
+// callbacks
+printScores();
