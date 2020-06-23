7c0e2ca28e117ae664e1d41583daac052c74e7e1
Bobbi Tarkany <bl.tarkany@gmail.com>
2020-06-22 18:07:41 -0700

    added game counters and html button grabs via element ID.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,14 @@
 // global variables
+// game counters
+var currentQuestionIndex = 0;
+var time = questions.length * 15;
+var timer;
+
 
 // html #id grab
+const start = document.getElementById('start');
+const submit = document.getElementById('submit');
+
 
 
 
